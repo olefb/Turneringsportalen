148809b95ccabbba935ee8dce785c3457d4e8cc9
Ivhene <ivarh.nesheim@gmail.com>
2025-02-25 18:57:39 +0100

    fix: moved the url and keys to environment variables

--- backend/src/main/kotlin/com/turneringsportalen/backend/configs/SupabaseConfig.kt
+++ backend/src/main/kotlin/com/turneringsportalen/backend/configs/SupabaseConfig.kt
@@ -5,15 +5,19 @@ import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
-class SupabaseConfig {
+class SupabaseConfig(private val env: Environment) {
 
     @Bean
-    fun client(): SupabaseClient { // Should probably be moved to environment variables or something
+    fun client(): SupabaseClient {
+        val supabaseUrl = env.getProperty("SUPABASE_URL") ?: throw RuntimeException("SUPABASE_URL not set")
+        val supabaseKey = env.getProperty("SUPABASE_KEY") ?: throw RuntimeException("SUPABASE_KEY not set")
+
         return createSupabaseClient(
-            supabaseUrl = "https://alkuvpbupekuvdgfvsnd.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa3V2cGJ1cGVrdXZkZ2Z2c25kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxMzg1MTQsImV4cCI6MjA1NTcxNDUxNH0.og6JYAkL2uoQ0OwSBXQmbVmu4aQUVxFexkl_kBR5RbY"
+            supabaseUrl = supabaseUrl,
+            supabaseKey = supabaseKey
         ) {
             install(Postgrest)
         }
