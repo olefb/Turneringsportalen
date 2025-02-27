296462b0f33f635f91046e1af278ee7bb86ea5d2
Brian.Jensvoll-Jorgensen <brian.jensvoll-jorgensen@student.uib.no>
2025-02-27 21:15:53 +0100

    feat: More functions/queries for TournamentService.kt

--- backend/src/main/kotlin/com/turneringsportalen/backend/services/TournamentService.kt
+++ backend/src/main/kotlin/com/turneringsportalen/backend/services/TournamentService.kt
@@ -1,40 +1,128 @@
 package com.turneringsportalen.backend.services
 
-import com.turneringsportalen.backend.entities.Tournament
+import com.turneringsportalen.backend.entities.*
 import org.springframework.stereotype.Service
 import io.github.jan.supabase.SupabaseClient
-import kotlinx.datetime.Clock
+import io.github.jan.supabase.postgrest.from
 import kotlinx.datetime.Instant
 
 @Service
 class TournamentService (private val client: SupabaseClient) {
-    // Currently empty, add methods about tournaments that interact with the database here
 
-    private val tournaments: MutableList<Tournament> = mutableListOf(
-        Tournament(1, "Name 1", Clock.System.now(), Clock.System.now(), "Location 1", listOf("Bane 1", "Bane 2", "Bane 3", "Bane 4"), "10:00"),
-        Tournament(2, "Name 2", Clock.System.now(), Clock.System.now(), "Location 1", listOf("Bane 1", "Bane 2", "Bane 3", "Bane 4"), "30:00"),
-        Tournament(3, "Name 3", Clock.System.now(), Clock.System.now(), "Location 3", listOf("Bane 1", "Bane 2", "Bane 3", ), "20:00"),
-    )
-
-    fun findAllTournaments(): List<Tournament>? {
-        return tournaments.toList()
+    // Function for the overarching algorithm of the app, automatically setting up a match schedule given a tournament, participants and playing fields exist.
+    // Gives back a list of matches so that the webpage can display them and so that changes might be made
+    suspend fun setUpMatches(tournamentId: Int) /*: List<Match>*/ {
+        // Todo: Set up this damn thing
     }
 
-    fun findById(id: Int): Tournament? {
-        return tournaments.find { it.tournamentId == id }
+    suspend fun createTournament(tournament: Tournament){
+        client.from("tournament").insert(tournament)
     }
 
-    fun save(tournament: Tournament): Tournament {
-        tournaments.add(tournament)
-        return tournament
+    suspend fun addParticipant(participant: Participant){
+        client.from("participant").insert(participant)
     }
 
-    fun delete(id: Int): Boolean {
-        return tournaments.removeIf { it.tournamentId == id }
+    suspend fun addMatch(match: Match, participants: List<Participant>){
+        client.from("match").insert(match)
+
+        for ((index, participant) in participants.withIndex()){
+            client.from("match_participant").insert(MatchParticipant(match.matchId, participant.participantId, index))
+        }
     }
 
-    fun update(tournament: Tournament): Tournament? {
-        delete(tournament.tournamentId)
-        return save(tournament)
+    suspend fun findAllTournaments(): List<Tournament>? {
+        return client.from("tournament").select().decodeList<Tournament>()
+    }
+
+    suspend fun findById(id: Int): Tournament? {
+        return client.from("tournament").select {
+            filter {
+                eq("tournament_id", id)
+            }
+        }.decodeSingle<Tournament>()
+    }
+
+    suspend fun findMatchById(id: Int): Match? {
+        return client.from("match").select {
+            filter {
+                eq("match_id", id)
+            }
+        }.decodeSingle<Match>()
+    }
+
+    suspend fun findMatchesByTournamentId(id: Int): List<Match>? {
+        return client.from("match").select {
+            filter {
+                eq("tournament_id", id)
+            }
+        }.decodeList<Match>()
+    }
+
+    suspend fun findFieldsByTournamentId(id: Int): List<TournamentField>? {
+        return client.from("available_fields").select {
+            filter {
+                eq("tournament_id", id)
+            }
+        }.decodeList<TournamentField>()
+    }
+
+    suspend fun findParticipantById(id: Int): Match? {
+        return client.from("participant").select {
+            filter {
+                eq("participant_id", id)
+            }
+        }.decodeSingle<Match>()
+    }
+
+    suspend fun findMatchParticipantByMatchId(id: Int): List<MatchParticipant>? {
+        return client.from("match_participant").select {
+            filter {
+                eq("participant_id", id)
+            }
+        }.decodeList<MatchParticipant>()
+    }
+
+    suspend fun deleteTournament(id: Int) {     // Change supabase so if a tournament is deleted it cascades
+        // Add Exceptions for when they should fail, currently only focused on getting it working
+        client.from("tournament").delete {
+            filter {
+                eq("tournament_id", id)
+            }
+        }
+    }
+
+    suspend fun deleteParticipant(id: Int) {     // Change supabase so if a tournament is deleted it cascades
+        // Add Exceptions for when they should fail, currently only focused on getting it working
+        client.from("participant").delete {
+            filter {
+                eq("participant_id", id)
+            }
+        }
+    }
+
+    suspend fun deleteMatch(id: Int) {     // Change supabase so if a tournament is deleted it cascades
+        // Add Exceptions for when they should fail, currently only focused on getting it working
+        client.from("match").delete {
+            filter {
+                eq("match_id", id)
+            }
+        }
+    }
+
+    suspend fun update(tournamentId : Int, name : String, startDate : Instant, location : String, matchInterval : Int) {
+        client.from("tournament").update(
+            {
+                set("tournament_id", tournamentId)
+                set("name", name)
+                set("start_date", startDate)
+                set("location", location)
+                set("match_interval", matchInterval)
+            }
+        ) {
+            filter {
+                eq("tournament_id", tournamentId)
+            }
+        }
     }
 }
