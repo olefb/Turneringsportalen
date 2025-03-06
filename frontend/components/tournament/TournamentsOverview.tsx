1d2be6f1c1253415531944f69fae6f7c45878efc
Alexander Fjellheim <alexglo@outlook.com>
2025-03-06 12:39:01 +0100

    feat/tournaments page (overview), removed test page and updated nav

---
+++ frontend/components/tournament/TournamentsOverview.tsx
@@ -0,0 +1,30 @@
+import { Container, Flex, Text, Strong, Heading, Box, Card, Avatar, Table, Grid } from "@radix-ui/themes";
+import { Tournament } from "@/utils/types";
+import { fetchTournaments } from "@/utils/API";
+
+
+export default async function TournamentsOverview() {
+    let tournaments: Tournament[] = await fetchTournaments();
+    console.log(tournaments);
+
+    return (
+        <Container>
+            <Grid gap="6" columns={{ xs: '1', md: '2', lg: '3' }}>
+                {tournaments.map((tournament) => 
+                <Box key={tournament.tournament_id}>
+                    <Card asChild>
+                        <a href={`/tournaments/${tournament.tournament_id}`}>
+                        <Heading as="h2">{tournament.name}</Heading>
+                            <Flex gap="1" direction={"column"}>
+                                <Text><Strong>Start Date: </Strong> {new Date(tournament.start_date).toLocaleDateString()}</Text>
+                                <Text><Strong>Location: </Strong> {tournament.location}</Text>
+                                <Text><Strong>Match interval: </Strong>{tournament.match_interval}</Text>
+                            </Flex>
+                        </a>
+                    </Card>
+                </Box>)}
+            </Grid>
+
+        </Container>
+    );
+}
