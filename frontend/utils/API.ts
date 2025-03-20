e7db37cf2e3704915a0e0857c9d4b9e52dcd8b47
Ole Birketvedt <fossbab@gmail.com>
2025-03-20 19:53:02 +0100

    Revert "chore: Get API URL from environment"
    This reverts commit e196d6a093f59cf709913fbf369ed9be07f68e39.

--- frontend/utils/API.ts
+++ frontend/utils/API.ts
@@ -4,7 +4,7 @@
 
 import { CreateTournamentDTO, Match, MatchParticipant } from "./types";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "http://localhost:8080";
 
 /**
  * Method that fetches the list of tournaments from the server
