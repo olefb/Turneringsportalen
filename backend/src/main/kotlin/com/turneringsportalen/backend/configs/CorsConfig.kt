4720c3ca4fe6ceb774c416946f5acd535d023905
Ole Birketvedt <fossbab@gmail.com>
2025-03-20 20:13:49 +0100

    test

--- backend/src/main/kotlin/com/turneringsportalen/backend/configs/CorsConfig.kt
+++ backend/src/main/kotlin/com/turneringsportalen/backend/configs/CorsConfig.kt
@@ -11,7 +11,7 @@ class CorsConfig {
     fun corsFilter(): CorsFilter {
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
-        config.allowedOrigins = listOf("https://app.vaffel.org") // Allow frontend
+        config.allowedOrigins = listOf("*") // Allow frontend
         config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all needed methods
         config.allowedHeaders = listOf("*") // Allow all headers
         config.allowCredentials = true // Allow credentials (if needed)
