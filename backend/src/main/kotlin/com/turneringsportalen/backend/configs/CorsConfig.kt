46d0e50116edac9d29c9d3f279c94fea7a50c54f
Ole Birketvedt <fossbab@gmail.com>
2025-03-20 21:03:54 +0100

    test

--- backend/src/main/kotlin/com/turneringsportalen/backend/configs/CorsConfig.kt
+++ backend/src/main/kotlin/com/turneringsportalen/backend/configs/CorsConfig.kt
@@ -1,10 +1,12 @@
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 
 @Configuration
+@CrossOrigin
 class CorsConfig {
 
     @Bean
